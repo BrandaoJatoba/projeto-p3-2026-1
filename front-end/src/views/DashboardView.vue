a24f413004a367ec538aefdda8d4fec83fdcbdb8
gabriella salvador <gabriellasalvador20@gmail.com>
2026-09-15 13:16:42 -0300

    PAP-17: controle de rotas privadas por perfil

---
+++ front-end/src/views/DashboardView.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <h1>Dashboard</h1>
+  <p>Essa página só pode ser vista por quem estiver logado e tiver o perfil permitido</p>
+</template>
+
+<style scoped></style>
